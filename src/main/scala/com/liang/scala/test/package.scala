76cd4527a45ca4c711a4f9b4792897b344677dcf
wuliangliang02 <wuliangliang02@meituan.com>
2017-02-15 10:11:25 +0800

    first commit

---
+++ src/main/scala/com/liang/scala/test/package.scala
@@ -0,0 +1,14 @@
+package com.liang.scala
+
+/**
+  * Created by doubling_ruc on 2017/2/14.
+  */
+package object test {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def test(): Unit ={
+
+  }
+}
